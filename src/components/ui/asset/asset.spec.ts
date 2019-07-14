51f50369f7606717f34f1e7fa3b4502f194c50ef
William M. Riley <mikeriley20@gmail.com>
2019-07-14 16:04:05 -0500

    Fix test

--- src/components/ui/asset/asset.spec.ts
+++ src/components/ui/asset/asset.spec.ts
@@ -12,9 +12,9 @@ it('should render and respond to changes appropriately', () => {
 
   let rendered = asset.render();
 
-  expect(rendered["$children$"][0]["$attrs$"]).toEqual({"aria-label": "create", "ariaLabel": "create", "name": "ios-ios-create", "src": undefined});
+  expect(rendered["$children$"][0]["$attrs$"]).toEqual({"aria-label": "create", "ariaLabel": "create", "name": "ios-create", "src": undefined});
 
-  expect(rendered["$children$"][0]["$name$"]).toEqual("ios-ios-create");
+  expect(rendered["$children$"][0]["$name$"]).toEqual("ios-create");
 
   asset.name = undefined;
   asset.src = "awesome";
