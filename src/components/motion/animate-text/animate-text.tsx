60869e38f44f253d6eb19493f12b716d779d341d
William M. Riley <mikeriley20@gmail.com>
2019-08-22 01:05:34 -0500

    a11y: make text readable on iOS voiceover

--- src/components/motion/animate-text/animate-text.tsx
+++ src/components/motion/animate-text/animate-text.tsx
@@ -8,15 +8,18 @@ import { animations } from "./animations";
 export class AnimateText {
   @Element() element: HTMLElement;
   @State() letters: NodeListOf<HTMLElement>;
+  @State() originalText: string;
   @Prop() method: string = "lettering";
   @Prop() words: boolean = false;
 
   componentWillLoad() {
+    this.originalText = this.element.textContent;
+
     if (this.words) {
-      this.element.innerHTML = this.element.textContent.replace(/[^, ]+/g, "<span class='letter'>$&</span>");
+      this.element.innerHTML = this.element.textContent.replace(/[^, ]+/g, "<span class='letter' aria-hidden='true'>$&</span>");
       this.letters = this.element.querySelectorAll('.letter');
     } else {
-      this.element.innerHTML = this.element.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+      this.element.innerHTML = this.element.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter' aria-hidden='true'>$&</span>");
       this.letters = this.element.querySelectorAll('.letter');
     }
   }
@@ -32,7 +35,7 @@ export class AnimateText {
   }
 
   render() {
-    return <Host>
+    return <Host role="text" ariaLabel={this.originalText}>
       <slot />
       <stellar-intersection element={this.element} multiple in={this.in.bind(this)} out={this.out.bind(this)} />
     </Host>
