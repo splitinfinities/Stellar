f8eddc91889d3d26749c9f9d94447418953ec566
William Riley <will@splitinfinities.com>
2020-01-15 18:27:24 -0600

    Update docs and committing updates

--- src/components/media/song/song.spec.ts
+++ src/components/media/song/song.spec.ts
@@ -5,7 +5,7 @@ describe('stellar-song', () => {
 	xit('should render and respond to changes appropriately', async () => {
 		const page = await newSpecPage({
 			components: [Song],
-			html: `	<stellar-song src="https://ui.splitinfinities.com/audio/playlist/01%20The%20Leaves%20Were%20Falling.mp3"></stellar-song>`,
+			html: `<stellar-song src="https://ui.splitinfinities.com/audio/playlist/01%20The%20Leaves%20Were%20Falling.mp3"></stellar-song>`,
 		});
 
 		expect(page.root).toEqualHtml(`
